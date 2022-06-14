775fada4e51b67f0cacb258ec8afdb64eed95965
Igor de Souza Bezerra <igorbezerra01@outlook.com>
2022-06-14 17:34:16 -0300

    feat: Hero repository

---
+++ src/main/java/dev/igor/javadocker/repository/HeroRepository.java
@@ -0,0 +1,9 @@
+package dev.igor.javadocker.repository;
+
+import dev.igor.javadocker.entity.Hero;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HeroRepository extends JpaRepository<Hero, Long> {
+}
